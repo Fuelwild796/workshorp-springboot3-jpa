e8dc7d94f879e590d503470ebc90ab3c8bd75b09
Roque <165859108+Fuelwild796@users.noreply.github.com>
2025-08-08 12:35:35 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/demo/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/demo/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.educandoweb.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoweb.demo.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/educandoweb/demo/resources/UserResource.java
+++ src/main/java/com/educandoweb/demo/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.educandoweb.demo.resources;
 
+import com.educandoweb.demo.repositories.UserRepository;
+import com.educandoweb.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.demo.entities.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService Service;
+
     //ResponseEntity -> Representa a resposta HTTP
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Roque","roque@gmail.com","799999999","123456");
-        return  ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = Service.findAll();
+        return  ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = Service.findById(id);
+        return  ResponseEntity.ok().body(obj);
     }
 
 }
