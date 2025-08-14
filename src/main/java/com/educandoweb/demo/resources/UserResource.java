8f8897917a0b9b35d07c941893479cd14cdb9ce8
Roque <165859108+Fuelwild796@users.noreply.github.com>
2025-08-14 17:18:07 -0300

    User insert

--- src/main/java/com/educandoweb/demo/resources/UserResource.java
+++ src/main/java/com/educandoweb/demo/resources/UserResource.java
@@ -4,13 +4,12 @@ import com.educandoweb.demo.repositories.UserRepository;
 import com.educandoweb.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.educandoweb.demo.entities.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +31,16 @@ public class UserResource {
         User obj = Service.findById(id);
         return  ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        User obj = Service.insert(user);
+        //comando para gerar a resposta 201 na criação de um novo objeto
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+
+
 
 }
